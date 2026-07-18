b2cd21a4e0ad0e00c0b8131e0079304e4bf4275e
Sophia S. <eroque-d@student.42sp.org.br>
2026-07-18 08:17:03 -0300

    refactor(libft): simplify ft_lstmap cleanup

--- libft/src/lib/ft_lstmap.c
+++ libft/src/lib/ft_lstmap.c
@@ -6,11 +6,17 @@
 /*   By: eroque-d <eroque-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 15:25:41 by eroque-d          #+#    #+#             */
-/*   Updated: 2026/07/02 12:16:33 by eroque-d         ###   ########.fr       */
+/*   Updated: 2026/07/18 08:17:03 by eroque-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static t_list	*clear_mapped_list(t_list **list, void (*del)(void *))
+{
+	ft_lstclear(list, del);
+	return (NULL);
+}
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
@@ -25,12 +31,12 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	{
 		new_content = f(lst->content);
 		if (!new_content)
-			return (ft_lstclear(&new_list, del), NULL);
+			return (clear_mapped_list(&new_list, del));
 		new_node = ft_lstnew(new_content);
 		if (!new_node)
 		{
 			del(new_content);
-			return (ft_lstclear(&new_list, del), NULL);
+			return (clear_mapped_list(&new_list, del));
 		}
 		ft_lstadd_back(&new_list, new_node);
 		lst = lst->next;
